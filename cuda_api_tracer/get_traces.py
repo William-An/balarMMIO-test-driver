b0e200fd5158cfd64381f6b2ad2e3dbb783944e4
Weili An <an107@purdue.edu>
2022-05-23 20:13:22 -0400

    Add opt to get trace tool

--- cuda_api_tracer/get_traces.py
+++ cuda_api_tracer/get_traces.py
@@ -6,6 +6,7 @@ GCC Version: 7.5.0
 
 """
 
+from email.policy import default
 import subprocess
 import logging
 import os, re, hashlib
@@ -18,14 +19,6 @@ try:
 except ImportError:
     from yaml import Loader, Dumper
 
-# Config yaml file path
-APPS_YAML_FILEPATH = "../define-all-apps.yml"
-CONFIGS_YAML_FILEPATH = "../define-standard-cfgs.yml"
-TRACES_FOLDER = "./hw_traces"
-TRACER_TOOL_SO = "./cuda_call_tracer/cuda_call_tracer.so"
-benchmark_suites_list = "rodinia_2.0-ft,GPU_Microbenchmark".split(",")
-benchmark_suites_list = [i.strip() for i in benchmark_suites_list]
-
 def get_argfoldername( args ):
     if args == "" or args == None:
         return "NO_ARGS"
@@ -36,27 +29,45 @@ def get_argfoldername( args ):
             foldername = "hashed_args_" + hashlib.md5(args).hexdigest()
         return foldername
 
-# parser = OptionParser()
-# parser.add_option("-B", "--benchmark_list", dest="benchmark_list",
-#                  help="a comma seperated list of benchmark suites to run. See ../define-all-apps.yml for " +\
-#                        "the benchmark suite names.",
-#                  default="rodinia_2.0-ft")
+parser = OptionParser()
+parser.add_option("-B", "--benchmark_list", dest="benchmark_list",
+                 help="a comma seperated list of benchmark suites to run. See ../define-all-apps.yml for " +\
+                       "the benchmark suite names.",
+                 default="rodinia_2.0-ft")
+parser.add_option("-l", "--loglevel", dest="loglevel", help="logging level, support: [debug, info, warning, error, critical]", default="info")
+parser.add_option("-t", "--trace_tool", dest="trace_tool", help="trace tool share object path", default="./cuda_call_tracer/cuda_call_tracer.so")
+parser.add_option("-o", "--output_folder", dest="output_folder", help="folder to put trace files", default="./hw_traces")
+parser.add_option("--apps_config", dest="apps_config", help="path to yaml config file of benchmark apps", default="../define-all-apps.yml")
+(options, args) = parser.parse_args()
+
+# Get arguments
+benchmark_suites_list = options.benchmark_list.split(",")
+benchmark_suites_list = [i.strip() for i in benchmark_suites_list]
+logging_level = options.loglevel.upper()
+tracer_tool_so = options.trace_tool
+trace_folder = os.path.abspath(options.output_folder)
+apps_yaml_config = os.path.abspath(options.apps_config)
 
 # Envs for launch
 cuda_version = os.getenv("CUDA_VERSION")
-cuda_api_tracer_tool = os.path.abspath(TRACER_TOOL_SO)
+cuda_api_tracer_tool = os.path.abspath(tracer_tool_so)
 
 
 # Logging config
 launch_counter = Counter()
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=getattr(logging, logging_level))
 
 # Benchmark runner
-benchmark_suites_table = yaml.load(open(APPS_YAML_FILEPATH), Loader=Loader)
+benchmark_suites_table = yaml.load(open(apps_yaml_config), Loader=Loader)
 top_dir = os.path.abspath(".")
 for benchmark_suite_name in benchmark_suites_list:
-    benchmark_suite_root = os.path.join(os.path.abspath(TRACES_FOLDER), benchmark_suite_name)
-    benchmark_suite = benchmark_suites_table[benchmark_suite_name]
+    benchmark_suite_root = os.path.join(trace_folder, benchmark_suite_name)
+
+    try:
+        benchmark_suite = benchmark_suites_table[benchmark_suite_name]
+    except KeyError:
+        logging.warning("benchmark suite {} does not exist!".format(benchmark_suite_name))
+        continue
     exec_dir = os.path.expandvars(benchmark_suite["exec_dir"])
     data_dir = os.path.expandvars(benchmark_suite["data_dirs"])
     benchmark_apps = benchmark_suite["execs"]
@@ -86,6 +97,12 @@ for benchmark_suite_name in benchmark_suites_list:
             if os.path.islink(app_sym_data_dir):
                 os.unlink(app_sym_data_dir)
             os.symlink(app_data_dir, app_sym_data_dir)
+
+            # Create softlinks to exe
+            app_symlink = os.path.join(run_dir, "run")
+            if os.path.islink(app_symlink):
+                os.unlink(app_symlink)
+            os.symlink(app_exec_path, app_symlink)
 
             # Launch shell script
             shell_script = "# Launch app: {} in benchmark suite: {}\n".format(app_name, benchmark_suite_name)
@@ -119,7 +136,7 @@ for benchmark_suite_name in benchmark_suites_list:
                 launch_counter["succ"] += 1
             else:
                 # Failure
-                logging.warning("app {} in benchmark suite {} failed to run with args: {}".format(app_name, benchmark_suite_name, res.args))
+                logging.warning("app {} in benchmark suite {} failed to run with args: {}".format(app_name, benchmark_suite_name, app_args))
                 launch_counter["fail"] += 1
 
             os.chdir(top_dir)
